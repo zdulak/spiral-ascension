f901744b8e80e1734b52814261881138c0c5ec59
zdulak <60813197+zdulak@users.noreply.github.com>
2022-04-14 00:00:44 +0200

    Recursive version

--- src/main/scala/SpiralAscension.scala
+++ src/main/scala/SpiralAscension.scala
@@ -1,7 +1,7 @@
 import scala.collection.mutable.ArrayBuffer
 
 object SpiralAscension extends App {
-  printSpiral(createSpiral(10))
+  printSpiral(createSpiral(3))
 
   def printSpiral(spiral: Seq[Seq[Int]]): Unit = {
     val maxLength = (spiral.size * spiral.size).toString.length
@@ -11,30 +11,15 @@ object SpiralAscension extends App {
     })
   }
 
-  def createSpiral(n: Int): Seq[Seq[Int]] = {
-    val startX = if (n % 2 == 0) n/ 2 else (1 - n) / 2
-    val startY = if (n % 2 == 0) (1 - n) / 2 else n / 2
-    val endX = if (n % 2 == 0) (1 - n) / 2 - 1 else n / 2 + 1
-    val endY = if (n % 2 == 0) n/ 2 + 1 else (1 - n) / 2 - 1
-    val dx = if (n % 2 == 0) -1 else 1
-    val dy = if (n % 2 == 0) 1 else -1
-    val reversedMatrix =
-      for (y <- startY until endY by dy) yield
-        for (x <- startX until endX by dx) yield
-          n * n - spiralIndex(x, y)
-    reversedMatrix.map(_.reverse).reverse
-  }
-
-  def  spiralIndex(x: Int, y: Int): Int = {
-    if (y * y >= x * x) {
-      val index = 4 * y * y - y - x
-      if (y < x) index - 2 * (y - x) else index
+  def createSpiral(size: Int): List[List[Int]] = {
+    def spiralHelper(numberLines: Int, lineLength: Int, start: Int): List[List[Int]] = {
+      if (numberLines == 0) Nil else {
+        (start until start + lineLength).toList ::
+        spiralHelper(lineLength, numberLines - 1, start + lineLength).reverse.transpose
+      }
     }
-    else {
-      val index = 4 * x * x - y - x
-      if (y < x) index + 2 * (y - x) else index
-    }
+
+    spiralHelper(size, size, 1)
   }
-
-
 }
+
